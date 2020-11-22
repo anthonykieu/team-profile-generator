33c33ff26acc4d9135d2f152b36bc38c451ad76e
Anthony H Kieu <anthonykieu1@Anthonys-MBP.fios-router.home>
2020-11-22 14:13:21 -0500

    refactor inquirer

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const inquirer = require('inquirer');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
-const { requiresArg } = require('yargs');
+// const { requiresArg } = require('yargs');
+// const { runInThisContext } = require('vm');
 
 // const { writeFile, copyFile } = require('./utils/generate-site.js');
 
-// const generatePage = require('./src/page-template');
-// const Manager = require('./lib/Manager.js');
+const generatePage = require('./src/page-template');
+
 
 const promptManagerTeam = () => {
     this.team = [];
@@ -46,35 +47,40 @@ const promptManagerTeam = () => {
                 type: 'input',
                 name: 'officeNumber',
                 message: 'What is your office number?'
-            }
+            },
         ])
-        .then(({ name, id, email, officeNumber }) => {
-            this.manager = new Manager(name, id, email, officeNumber);
-            console.log(this.manager.getMgrInfo());
+        .then(({ name, id, email, role, officeNumber }) => {
+            this.manager = new Manager(name, id, email, role, officeNumber);
+            // console.log(this.manager.getMgrInfo());
             this.team.push(this.manager);
-            console.log(this.team);
+            // console.log(this.team);
             promptMemberTeam()
         })
-    // .then(promptMemberTeam)
-    // .then(promptProject)
 };
 
 const promptMemberTeam = () => {
     inquirer
         .prompt({
             type: 'list',
-            message: 'Which team member would you like to add?',
-            name: 'role',
-            choices: ['engineer', 'intern', 'none']
+            message: 'Would you like to add team member?',
+            name: 'addMember',
+            choices: ['Yes', 'No']
+
         })
-        .then(({ role }) => {
-            if (role === 'engineer') {
+        .then(({ addMember }) => {
+            if (addMember === 'Yes') {
                 inquirer
                     .prompt([
                         {
+                            type: 'list',
+                            message: 'Which team member would you like to add?',
+                            name: 'role',
+                            choices: ['engineer', 'intern']
+                        },
+                        {
                             type: 'input',
                             name: 'name',
-                            message: 'Name of engineeer',
+                            message: 'Name of team member',
                             validate: nameInput => {
                                 if (nameInput) {
                                     return true;
@@ -97,59 +103,39 @@ const promptMemberTeam = () => {
                         {
                             type: 'input',
                             name: 'github',
-                            message: 'What is the link to guthub empployee site?'
-                        }
-
-                    ])
-                    .then(({ name, id, email, role, github }) => {
-                        this.engineer = new Engineer(name, id, email, role, github);
-                        console.log(this.engineer.getEngInfo());
-                        this.team.push(this.engineer);
-                        console.log(this.team);
-                        promptMemberTeam()
-                    })
-            } if (role === 'intern') {
-                inquirer
-                    .prompt([
-                        {
-                            type: 'input',
-                            name: 'name',
-                            message: 'Name of intern',
-                            validate: nameInput => {
-                                if (nameInput) {
-                                    return true;
-                                } else {
-                                    console.log('Please enter name!');
-                                    return false;
-                                }
-                            }
-                        },
-                        {
-                            type: 'input',
-                            name: 'id',
-                            message: 'What is employee identification number?'
-                        },
-                        {
-                            type: 'input',
-                            name: 'email',
-                            message: 'What is employee email?'
+                            message: 'What is the link to guthub empployee site?',
+                            when: (answers) => answers.role === 'engineer'
                         },
                         {
                             type: 'input',
                             name: 'school',
-                            message: 'What is the school intern is attending?'
+                            message: 'What is the school intern is attending?',
+                            when: (answers) => answers.role === 'intern'
                         }
 
                     ])
-                    .then(({ name, id, email, role, school }) => {
-                        this.intern = new Intern(name, id, email, role, school);
-                        console.log(this.intern.getInternInfo());
-                        this.team.push(this.intern);
-                        console.log(this.team);
-                        promptMemberTeam()
+                    .then(({ name, id, email, role, github, school }) => {
+                        if (role === 'engineer') {
+                            this.engineer = new Engineer( name, id, email, role, github, school );
+                            // console.log(this.engineer.getEngInfo());
+                            this.team.push(this.engineer);
+                            // console.log(this.team);
+                            promptMemberTeam()
+                        } else {
+                            this.intern = new Intern(name, id, email, role, school);
+                            // console.log(this.intern.getInternInfo());
+                            this.team.push(this.intern);
+                            // console.log(this.team);
+                            promptMemberTeam()
+                        }
                     })
+
             } else {
                 console.log('Your team page has been generated');
+                teamArray = this.team
+                // console.log(teamArray);
+                console.log(teamArray);
+                return teamArray;
             }
 
         });
